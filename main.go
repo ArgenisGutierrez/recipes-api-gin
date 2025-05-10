ecebdd25f479c75807975410f2461a7bad4e960f
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2025-05-10 13:40:52 -0600

    doc:openapi with swagger
    se creo la documentacion de forma automatica con swagger

--- main.go
+++ main.go
@@ -1,4 +1,21 @@
-// Package main API para gestionar recetas
+// Recipes API
+//
+// This is a sample recipes API. You can find out more about the API at https://github.com/ArgenisGutierrez/recipes-api-gin
+//
+// Schemes: http
+//
+// Host: localhost:8080
+// BasePath: /
+// Version: 1.0.0
+// Contact: Argenis Gutierrez <argenis.v.gtz@gmail.com> https://github.com/ArgenisGutierrez
+// License: MIT https://opensource.org/licenses/MIT
+//
+// Consumes:
+// - application/json
+//
+// Produces:
+// - application/json
+// swagger:meta
 package main
 
 import (
@@ -30,7 +47,17 @@ func init() {
 	_ = json.Unmarshal([]byte(file), &recipes)
 }
 
-// NewRecipeHandler crear una receta
+// swagger:operation POST /recipes recipes newRecipe
+// Returns a NewRecipeHandler
+// ---
+// produces:
+// - application/json
+// responses:
+//
+//	'200':
+//		description: Successful operation
+//	'400':
+//		description: Invalid data
 func NewRecipeHandler(ctx *gin.Context) {
 	var recipe Recipe
 	if err := ctx.ShouldBindJSON(&recipe); err != nil {
@@ -44,11 +71,39 @@ func NewRecipeHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, recipe)
 }
 
-// ListRecipesHandler Listar recetas
+// swagger:operation GET /recipes recipes listRecipes
+// Returns list of recipes
+// ---
+// produces:
+// - application/json
+// responses:
+// '200':
+// description: Successful operation
 func ListRecipesHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, recipes)
 }
 
+// swagger:operation PUT /recipes/{id} recipes updateRecipe
+// Returns a updated recipe
+// ---
+// parameters:
+//   - name: id
+//     in: path
+//     description: ID of the recipe to update
+//     required: true
+//     type: string
+//
+// produces:
+//   - application/json
+//
+// responses:
+//
+//	'200':
+//	  description: Operación exitosa
+//	'400':
+//	  description: ID inválido
+//	'404':
+//	  description: Receta no encontrada
 func UpdateRecipeHandler(ctx *gin.Context) {
 	id := ctx.Params.ByName("id")
 	var recipe Recipe
@@ -78,6 +133,25 @@ func UpdateRecipeHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, recipe)
 }
 
+// swagger:operation DELETE /recipes/{id} recipes deleteRecipe
+// returns a deleted recipe
+// ---
+// parameters:
+//   - name: id
+//     in: path
+//     description: ID de la receta
+//     required: true
+//     type: string
+//
+// produces:
+//   - application/json
+//
+// responses:
+//
+//	'200':
+//		description: Successful operation
+//	'404':
+//		description: Recipe not found
 func DeleteRecipeHandler(ctx *gin.Context) {
 	id := ctx.Params.ByName("id")
 	index := -1
@@ -100,6 +174,22 @@ func DeleteRecipeHandler(ctx *gin.Context) {
 	})
 }
 
+// swagger:operation GET /recipes/search recipes searchRecipes
+// returns a list of recipes
+// ---
+// parameters:
+//   - name: tag
+//     in: query
+//     description: Tag para buscar
+//     required: true
+//     type: string
+//
+// responses:
+//
+//	'200':
+//		description: Successful operation
+//	'404':
+//		description: No recipes found
 func SearchRecipesHandler(ctx *gin.Context) {
 	tag := ctx.Query("tag")
 	listOfRecipes := make([]Recipe, 0)
